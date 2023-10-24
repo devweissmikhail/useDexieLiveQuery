ca86b987c4b77a7e991a20e1b112bed15dd3e70b
Weiss <55186323+devweissmikhail@users.noreply.github.com>
2023-10-24 23:23:31 +0300

    Add files via upload

---
+++ useDexieLiveQuery.ts
@@ -0,0 +1,62 @@
+import { liveQuery, type Subscription } from "dexie";
+import { shallowRef, watch, type ShallowRef } from "vue";
+import { tryOnScopeDispose } from "@vueuse/core";
+
+
+type Value<T, I> = I extends undefined ? T | undefined : T | I;
+
+type Options<I> = {
+  onError?: (error: any) => void;
+  initialValue?: I;
+  deps?: any;
+};
+
+
+export function useDexieLiveQuery<
+  T,
+  I = undefined,
+>(
+  querier: () => T | Promise<T>,
+  options?: Options<I>,
+): ShallowRef<Value<T, I>> {
+
+  const value = shallowRef<T | I | undefined>(options?.initialValue);
+
+  let subscription: Subscription | undefined = undefined;
+
+
+  tryOnScopeDispose(() => {
+    subscription?.unsubscribe();
+
+    // Set to undefined to avoid calling unsubscribe multiple times on a same subscription
+    subscription = undefined;
+  });
+
+
+  function start() {
+    subscription?.unsubscribe();
+
+    const observable = liveQuery(querier);
+
+    subscription = observable.subscribe({
+      next: result => {
+        value.value = result;
+      },
+      error: error => {
+        options?.onError?.(error);
+      },
+    });    
+  }
+
+
+  if (options?.deps)
+    watch(options.deps, () => {
+      start();
+    }, { immediate: true });
+  else
+    start();
+
+
+  return value as ShallowRef<Value<T, I>>;
+
+}
